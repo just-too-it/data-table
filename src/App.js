c2df870e18ee602e238f3bcccedec56f73500acd
Natasha <just.too.it@gmail.com>
2022-05-10 12:13:50 +0300

    добавлен роутинг, рефакторинг архитектуры

--- src/App.js
+++ src/App.js
@@ -1,57 +1,13 @@
-import { useEffect, useState, useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import { Pagination } from './components/Pagination';
-import { Search } from './components/Search';
-import { Table } from './components/Table';
-import { getPosts } from './core/services/posts.service';
-import { getNumberPages } from './core/utils/pages';
+import { AppRouter } from './routes';
 
 export const App = () => {
-  const [posts, setPosts] = useState(null);
-  const [pagesCount, setPagesCount] = useState(0);
-  const registry = useSelector((state) => state.registry);
-  const { postsPerPage, currentPage, searchQuery, sortBy, sortOrderById, sortOrderByTitle, sortOrderByBody } = registry;
-  let [sortOrder, setSortOrder] = useState(sortOrderById);
-  const navigate = useNavigate();
-
-  useMemo(() => {
-    switch (sortBy) {
-      case 'id':
-        setSortOrder(sortOrderById);
-        break;
-      case 'title':
-        setSortOrder(sortOrderByTitle);
-        break;
-      case 'body':
-        setSortOrder(sortOrderByBody);
-        break;
-      default:
-        break;
-    }
-  }, [sortBy, sortOrderById, sortOrderByTitle, sortOrderByBody]);
-
-  async function fetchPosts() {
-    const posts = await getPosts(postsPerPage, currentPage, searchQuery, sortBy, sortOrder);
-    setPosts(posts.data);
-    const totalCount = posts.headers['x-total-count'];
-    setPagesCount(getNumberPages(totalCount, postsPerPage));
-  }
-
-  useEffect(() => {
-    void fetchPosts();
-  }, [currentPage, searchQuery, sortBy, sortOrder]);
-
-  useEffect(() => {
-    navigate(`?page=${currentPage}`);
-  }, [currentPage]);
-
   return (
     <div className="container">
-      <Search />
-      {posts ? <Table posts={posts} postsPerPage={postsPerPage} /> : null}
-      <Pagination currentPage={currentPage} pagesCount={pagesCount} />
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 import { store } from './store';
@@ -10,10 +9,8 @@ import './styles/styles.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
